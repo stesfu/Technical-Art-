1c4059db42eaacc137f787cc68059c3686418ed4
stesfu <salemtesfu@gmail.com>
2020-07-22 15:05:00 -0500

    added counter to add more keyframes

--- random_animation.py
+++ random_animation.py
@@ -10,29 +10,33 @@ cone = maya.cmds.polyCone() [0]
 startTime = maya.cmds.playbackOptions(query = True, min = True)
 endTime = maya.cmds.playbackOptions(query = True, max = True)
 
+# Set current time to time slider start
+maya.cmds.currentTime(startTime)
 
-
+# Create list and set counter 
 shapes = [cube, sphere, cone]
+counter = 0
 
-for shape in shapes:
-    # Set current time to time slider start
-    maya.cmds.currentTime(startTime)
-    
-    # Select shape
-    maya.cmds.select(shape, r = True)
-    
-    # Set the keyframe
-    maya.cmds.setKeyframe(shape)
-    
-    # Edit the current time
-    maya.cmds.currentTime(random.randint(startTime, endTime))
-    
-    # Move the shape 
-    maya.cmds.setAttr(shape + ".translateX", random.randint(0,10))
-    maya.cmds.setAttr(shape + ".translateY", random.randint(0,10))
-    maya.cmds.setAttr(shape + ".translateZ", random.randint(0,10))
-    
-    # Set another keyframe
-    maya.cmds.setKeyframe()
+while counter < 5:
+
+    for shape in shapes:
+        
+        # Select shape
+        maya.cmds.select(shape, r = True)
+        
+        # Set the keyframe
+        maya.cmds.setKeyframe(shape)
+        
+        # Edit the current time
+        maya.cmds.currentTime(random.randint(startTime, endTime))
+        
+        # Move the shape 
+        maya.cmds.setAttr(shape + ".translateX", random.randint(0,15))
+        maya.cmds.setAttr(shape + ".translateY", random.randint(0,15))
+        maya.cmds.setAttr(shape + ".translateZ", random.randint(0,15))
+        
+        # Set another keyframe
+        maya.cmds.setKeyframe()
+    counter += 1
 
 
